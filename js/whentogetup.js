83c12192c5655111cb5346883484f9ba95bdd41d
Greg Karékinian <greg@karekinian.com>
2013-11-05 11:11:15 +0100

    Switch to a saner style of spacing

--- js/whentogetup.js
+++ js/whentogetup.js
@@ -1,28 +1,29 @@
-(function(){
-  var now = function(){
+(function() {
+  var now = function() {
     return new Date();
   }
-  var calculateTime = function(cycles){
-    return new Date(now().getTime() + (14 + 90*cycles) * 60000);
+
+  var calculateTime = function(cycles) {
+    return new Date(now().getTime() + (14 + 90 * cycles) * 60000);
   };
 
-  var toTwoDidgets = function(v){
-    if(v < 10){
-      return "0"+v;
-    }else{
-      return ""+v;
+  var toTwoDidgets = function(v) {
+    if (v < 10) {
+      return "0" + v;
+    } else {
+      return "" + v;
     }
   }
 
-  var formatDate = function(date){
+  var formatDate = function(date) {
     return toTwoDidgets(date.getHours()) + ":" + toTwoDidgets(date.getMinutes());
   };
 
-  var updateTimes = function(){
+  var updateTimes = function() {
     var i, element;
-    for(i = 2; i <=7; i = i + 1){
-      element = document.getElementById("result-with-"+i+"-cycles");
-      if(element){
+    for (i = 2; i <=7; i = i + 1) {
+      element = document.getElementById("result-with-" + i + "-cycles");
+      if (element) {
         element.innerHTML = formatDate(calculateTime(i));
       }
     }
